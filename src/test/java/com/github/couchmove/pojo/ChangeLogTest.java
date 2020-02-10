51edea55a83ae871b0919520a4381fbf54ce64bc
Jesus The Hun <iletaitunefoisfenghuang@gmail.com>
2020-02-10 17:04:33 +0100

    make ChangeLog version compatible with semver convention (#3)

---
+++ src/test/java/com/github/couchmove/pojo/ChangeLogTest.java
@@ -0,0 +1,20 @@
+package com.github.couchmove.pojo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChangeLogTest {
+
+    @Test
+    public void should_compare_version_properly() {
+
+        ChangeLog changeLog1 = ChangeLog.builder().version("0.9").build();
+        ChangeLog changeLog2 = ChangeLog.builder().version("0.11").build();
+        ChangeLog changeLog3 = ChangeLog.builder().version("0.9").build();
+
+        assertEquals(-1, changeLog1.compareTo(changeLog2));
+        assertEquals(1, changeLog2.compareTo(changeLog1));
+        assertEquals(0, changeLog1.compareTo(changeLog3));
+    }
+}
