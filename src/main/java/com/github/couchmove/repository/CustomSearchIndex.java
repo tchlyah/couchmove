0a3b44888d75206b46468d67ba4183ed6f846835
coderguy-tech <george.kysaad@gmail.com>
2021-06-09 22:30:01 -0400

    Json fix for CustomSearchIndex

--- src/main/java/com/github/couchmove/repository/CustomSearchIndex.java
+++ src/main/java/com/github/couchmove/repository/CustomSearchIndex.java
@@ -1,15 +1,26 @@
-package tech.jhipster.sample.config.couchbase;
+package com.github.couchmove.repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import com.couchbase.client.java.manager.search.SearchIndex;
+import com.couchbase.client.core.deps.com.fasterxml.jackson.annotation.JsonCreator;
+import com.couchbase.client.core.deps.com.fasterxml.jackson.annotation.JsonProperty;
 import com.couchbase.client.core.json.Mapper;
 
 public class CustomSearchIndex extends SearchIndex {
 
-    public CustomSearchIndex(String uuid, String name, String type, Map<String, Object> params, String sourceUuid,
-            String sourceName, Map<String, Object> sourceParams, String sourceType, Map<String, Object> planParams) {
+    @JsonCreator
+    public CustomSearchIndex(
+        @JsonProperty("uuid") String uuid,
+        @JsonProperty("name") String name,
+        @JsonProperty("type") String type,
+        @JsonProperty("params") Map<String, Object> params,
+        @JsonProperty("sourceUUID") String sourceUuid,
+        @JsonProperty("sourceName") String sourceName,
+        @JsonProperty("sourceParams") Map<String, Object> sourceParams,
+        @JsonProperty("sourceType") String sourceType,
+        @JsonProperty("planParams") Map<String, Object> planParams) {
         super(uuid, name, type, params, sourceUuid, sourceName, sourceParams, sourceType, planParams);
     }
 
