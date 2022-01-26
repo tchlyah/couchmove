c55d114668d716f022431412b05a3660cc3c89f7
Tayeb Chlyah <tayebchlyah@gmail.com>
2022-01-26 16:39:26 +0100

    🐛 Fetch files with same version

--- src/test/java/com/github/couchmove/service/ChangeLogFileServiceTest.java
+++ src/test/java/com/github/couchmove/service/ChangeLogFileServiceTest.java
@@ -3,6 +3,8 @@ package com.github.couchmove.service;
 import com.github.couchmove.exception.CouchmoveException;
 import com.github.couchmove.pojo.ChangeLog;
 import com.github.couchmove.pojo.Type;
+import com.google.common.io.Files;
+import lombok.var;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.github.couchmove.utils.TestUtils.getRandomString;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -62,36 +65,72 @@ public class ChangeLogFileServiceTest {
     @Test
     public void should_fetch_changeLogs() throws IOException {
         List<ChangeLog> changeLogs = Stream.of(
-                ChangeLog.builder()
-                        .type(Type.N1QL)
-                        .script("V0__create_index.n1ql")
-                        .version("0")
-                        .description("create index")
-                        .checksum("1a417b9f5787e52a46bc65bcd801e8f3f096e63ebcf4b0a17410b16458124af3")
-                        .build(),
-                ChangeLog.builder()
-                        .type(Type.DOCUMENTS)
-                        .script("V0.1__insert_users")
-                        .version("0.1")
-                        .description("insert users")
-                        .checksum("99a4aaf12e7505286afe2a5b074f7ebabd496f3ea8c4093116efd3d096c430a8")
-                        .build(),
-                ChangeLog.builder()
-                        .type(Type.DESIGN_DOC)
-                        .script("V1__user.json")
-                        .version("1")
-                        .description("user")
-                        .checksum("22df7f8496c21a3e1f3fbd241592628ad6a07797ea5d501df8ab6c65c94dbb79")
-                        .build(),
-                ChangeLog.builder()
-                        .type(Type.FTS)
-                        .script("V2__name.fts")
-                        .version("2")
-                        .description("name")
-                        .checksum("6ef9c3cc661804f7f0eb489e678971619a81b5457cff9355e28db9dbf835ea0a")
-                        .build())
+                        ChangeLog.builder()
+                                .type(Type.N1QL)
+                                .script("V0__create_index.n1ql")
+                                .version("0")
+                                .description("create index")
+                                .checksum("1a417b9f5787e52a46bc65bcd801e8f3f096e63ebcf4b0a17410b16458124af3")
+                                .build(),
+                        ChangeLog.builder()
+                                .type(Type.DOCUMENTS)
+                                .script("V0.1__insert_users")
+                                .version("0.1")
+                                .description("insert users")
+                                .checksum("99a4aaf12e7505286afe2a5b074f7ebabd496f3ea8c4093116efd3d096c430a8")
+                                .build(),
+                        ChangeLog.builder()
+                                .type(Type.DESIGN_DOC)
+                                .script("V1__user.json")
+                                .version("1")
+                                .description("user")
+                                .checksum("22df7f8496c21a3e1f3fbd241592628ad6a07797ea5d501df8ab6c65c94dbb79")
+                                .build(),
+                        ChangeLog.builder()
+                                .type(Type.FTS)
+                                .script("V2__name.fts")
+                                .version("2")
+                                .description("name")
+                                .checksum("6ef9c3cc661804f7f0eb489e678971619a81b5457cff9355e28db9dbf835ea0a")
+                                .build())
                 .collect(Collectors.toList());
         Assert.assertEquals(changeLogs, new ChangeLogFileService("db/migration/success").fetch());
+    }
+
+    @Test
+    public void should_fetch_files_with_same_version() throws IOException {
+        // Given a temp directory that contains
+        File tempDir = Files.createTempDir();
+        tempDir.deleteOnExit();
+
+        // Create files with same version
+        File file1 = File.createTempFile("V1__description_1", ".json", tempDir);
+        String content1 = "content1";
+        Files.write(content1.getBytes(), file1);
+        // n1ql file
+        File file2 = File.createTempFile("V1__description_2", ".n1ql", tempDir);
+        String content2 = "content2";
+        Files.write(content2.getBytes(), file2);
+
+        // When we fetch changelogs in this directory
+        var results = new ChangeLogFileService(tempDir.toPath().toString()).fetch();
+
+        // Then we should get have this changelogs in the same order
+        assertThat(results).hasSize(2);
+        assertThat(results)
+                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("checksum", "description")
+                .containsExactlyInAnyOrder(
+                        ChangeLog.builder()
+                                .type(Type.DESIGN_DOC)
+                                .script(file1.getName())
+                                .version("1")
+                                .build(),
+                        ChangeLog.builder()
+                                .type(Type.N1QL)
+                                .script(file2.getName())
+                                .version("1")
+                                .build()
+                );
     }
 
 }
